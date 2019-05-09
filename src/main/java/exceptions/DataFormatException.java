d7a5b6d65af5834b68640f7c36b6f6404d19a22f
MXuDong <2941884109@qq.com>
2019-05-09 14:52:02 +0800

    增加异常:DataFormatException

---
+++ src/main/java/exceptions/DataFormatException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+/**
+ * @author Dong
+ * <p>
+ * Class Name : DataFormatException
+ * Create Time : 14:50
+ * Create Date : 2019/5/9
+ * Project : WebApi
+ */
+
+public class DataFormatException extends Exception {
+    public DataFormatException(String msg){
+        super(msg);
+    }
+}
